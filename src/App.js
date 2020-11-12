3728d36a910a36b40f04fe7bd413f12783a6dd95
RamshaZulfiqar <73095632+RamshaZulfiqar@users.noreply.github.com>
2020-11-12 22:19:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React What is this.
+          Ramsha What is this.
         </a>
       </header>
     </div>
